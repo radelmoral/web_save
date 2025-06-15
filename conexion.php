b8e38fc89fee3ec7ec446dc1aa58d39d8b8c6258
radelmoral <rafael.delmoraljimenez@gmail.com>
2025-06-15 15:35:00 +0200

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 function devolverConexion()
 {
-    $server="webpvp_mariadb:3306";
+    $server="loclahost:3306";
     $usuario="save";
     $clave="46M1!!Jo57wU.aai";
     $base="Repuestos";
